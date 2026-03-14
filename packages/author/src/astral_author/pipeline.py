f0acaf64e96d685befc795c7a8bc2af66f10f190
Aaron Taylor <git@ataylor.io>
2026-03-14 15:59:43 -0700

    Revert "autoiterate #8: increase max_deep_dives to 8, min_group_size to 1"
    This reverts commit d438f629dcbbb5ab1c525b48c08ad45767e613a6.

--- packages/author/src/astral_author/pipeline.py
+++ packages/author/src/astral_author/pipeline.py
@@ -107,7 +107,7 @@ def _build_baseline() -> DraftPipeline:
     return DraftPipeline(
         name="baseline",
         ranker=EngagementRanker(),
-        clusterer=CategoryClusterer(max_deep_dives=8, min_group_size=1),
+        clusterer=CategoryClusterer(max_deep_dives=5),
         summarizer=LLMSummarizer(),
         drafter=MarkdownDrafter(),
     )
